fc952479c6d4f249a467ba70eab63983c879b925
pieewiee <till.niederauer@googlemail.com>
2021-05-16 18:13:55 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,29 +1,19 @@
 <?php
-
+    // outputs image directly into browser, as PNG stream
     include('phpqrcode/qrlib.php');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $Content = file_get_contents('php://input');
+        QRCode::png($Content);
+        exit;
+    }
+
     if(isset($_GET['content'])) {
         $Content = $_GET['content'];
-        // outputs image directly into browser, as PNG stream
         QRCode::png($Content);
     } 
-
-    if(isset($_POST["content"])) {
-        // outputs image directly into browser, as PNG stream
-        QRCode::png($Content);
-    }
-    
-    elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        print_r($_POST["content"]);
-    }
     else {
         $Content = 'Read the documentation';
         QRCode::png($Content);
     }
-
-
-
 ?>
-
-
-$inputJSON = file_get_contents('php://input');
-$input = json_decode($inputJSON, TRUE); //convert JSON into array
